ea268cba6caa70ff716c65fd1330ea3eb1e4c7b1
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-04-15 21:34:21 +0700

    Custom module 2 v1.0

--- wordpress/wp-content/themes/twentytwenty/template-parts/content.php
+++ wordpress/wp-content/themes/twentytwenty/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The default template for displaying content
  *
@@ -16,51 +17,94 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
+	// get_template_part( 'template-parts/entry-header' );
+	?>
+<?php
+// Nếu đây là trang home sẽ đi vào trong đây
+ if (is_home()) {
+	?>
+	<div class="container">
+		<div class="content_box_m">
+			<div class="row">
+				<div class="col-md-3 col-xs-3 topnewstime">
+					<span class="topnewsdate"><?php echo get_the_date('d', get_the_ID()); ?></span><br>
+					<span class="topnewsmonth">Tháng <?php echo get_the_date('m', get_the_ID()); ?></span><br>
+				</div>
+				<div class="col-md-9 col-xs-9 shortdesc">
+					<?php
+					if (is_singular()) {
+						the_title('<h1 class="entry-title">', '</h1>');
+					} else {
+						// this is a title in content
+						the_title('<h4><a href="' . esc_url(get_permalink()) . '">', '</a></h4>');
+					}
+					?>
+					<p class="p">
+						<?php
+						the_content(__('Continue reading', 'twentytwenty'));
+						// edit_post_link();
+						?>
+					</p>
+				</div>
 
-	get_template_part( 'template-parts/entry-header' );
 
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
+			</div>
+		</div>
+	</div>
+<?php } ?>
+
+	<?php
+	if (!is_search()) {
+		get_template_part('template-parts/featured-image');
 	}
 
 	?>
 
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+<?php
+// Nếu đây không phải trang home sẽ đi vào trong đây
+ if (!is_home()) {
+	?>
+
+
+	<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
 		<div class="entry-content">
 
 			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
+			if (is_singular()) {
+				the_title('<h1 class="entry-title">', '</h1>');
 			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
+				// this is a title in content
+				the_title('<h4><a href="' . esc_url(get_permalink()) . '">', '</a></h4>');
 			}
 			?>
 
-		</div><!-- .entry-content -->
+		</div>
+		<!-- .entry-content -->
 
-	</div><!-- .post-inner -->
-
+	</div>
+	<!-- .post-inner -->
+<?php } ?>
 	<div class="section-inner">
 		<?php
 		wp_link_pages(
 			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
 				'after'       => '</nav>',
 				'link_before' => '<span class="page-number">',
 				'link_after'  => '</span>',
 			)
 		);
 
-		edit_post_link();
+		//This is link edit of post
+		// edit_post_link();
 
 		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+		twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
 
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+		if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
 
-			get_template_part( 'template-parts/entry-author-bio' );
-
+			get_template_part('template-parts/entry-author-bio');
 		}
 		?>
 
@@ -68,18 +112,17 @@
 
 	<?php
 
-	if ( is_single() ) {
+	if (is_single()) {
 
-		get_template_part( 'template-parts/navigation' );
-
+		get_template_part('template-parts/navigation');
 	}
 
 	/*
 	 * Output comments wrapper if it's a post, or if comments are open,
 	 * or if there's a comment number – and check for password.
 	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-		?>
+	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+	?>
 
 		<div class="comments-wrapper section-inner">
 
@@ -87,7 +130,7 @@
 
 		</div><!-- .comments-wrapper -->
 
-		<?php
+	<?php
 	}
 	?>
 
