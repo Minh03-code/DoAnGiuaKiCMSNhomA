45936030476d5c1b3dc9257c14f6b6435b9933cd
linh1006 <21211tt4494@mail.tdc.edu.vn>
2023-04-19 14:32:21 +0700

    Update file footer

--- wordpress/wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wordpress/wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -13,9 +13,11 @@ $has_social_menu = has_nav_menu( 'social' );
 
 $has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
 $has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+$has_sidebar_3 = is_active_sidebar( 'sidebar-3' );
 
 // Only output the container if there are elements to display.
-if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) {
+if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 || $has_sidebar_3 ) {
+	
 	?>
 
 	<div class="footer-nav-widgets-wrapper header-footer-group">
@@ -23,7 +25,6 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 		<div class="footer-inner section-inner">
 
 			<?php
-
 			$footer_top_classes = '';
 
 			$footer_top_classes .= $has_footer_menu ? ' has-footer-menu' : '';
@@ -83,34 +84,8 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
 				</div><!-- .footer-top -->
 
 			<?php } ?>
-
-			<?php if ( $has_sidebar_1 || $has_sidebar_2 ) { ?>
-
-				<aside class="footer-widgets-outer-wrapper">
-
-					<div class="footer-widgets-wrapper">
-
-						<?php if ( $has_sidebar_1 ) { ?>
-
-							<div class="footer-widgets column-one grid-item">
-								<?php dynamic_sidebar( 'sidebar-1' ); ?>
-							</div>
-
-						<?php } ?>
-
-						<?php if ( $has_sidebar_2 ) { ?>
-
-							<div class="footer-widgets column-two grid-item">
-								<?php dynamic_sidebar( 'sidebar-2' ); ?>
-							</div>
-
-						<?php } ?>
-
-					</div><!-- .footer-widgets-wrapper -->
-
-				</aside><!-- .footer-widgets-outer-wrapper -->
-
-			<?php } ?>
+<!-- Footer Tim Duoc Nam O Day -->
+			
 
 		</div><!-- .footer-inner -->
 
