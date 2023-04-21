44656d95a280f07bff66eef79d10763e28a1dd09
phucnh0707 <nguyenhphuc0707@gmail.com>
2023-04-21 18:21:14 +0700

    Update

--- wordpress/wp-content/themes/twentytwenty/header.php
+++ wordpress/wp-content/themes/twentytwenty/header.php
@@ -16,13 +16,15 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 <head>
-
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
+
+	<!-- Link is Header Moudel 1 -->
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<?php wp_head(); ?>
+	
 
 </head>
 
@@ -41,7 +43,7 @@
 		?>
 
 
-		
+
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
