28cd6d80a2d7f04cec081af40791b24bc4e395d0
linh1006 <21211tt4494@mail.tdc.edu.vn>
2023-04-21 15:30:22 +0700

    Format

--- wordpress/wp-content/themes/twentytwenty/header.php
+++ wordpress/wp-content/themes/twentytwenty/header.php
@@ -44,8 +44,8 @@
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<!------ Include the above in your HEAD tag ---------->
 
 		<nav class="navbar navbar-icon-top navbar-default">
@@ -127,11 +127,11 @@
 								<li><a href="<?php echo wp_logout_url(); ?>">Loguot</a></li>
 							</ul>
 						</li>
-						
+
 					</ul>
-				
+
 				</div><!-- /.navbar-collapse -->
-				
+
 			</div><!-- /.container-fluid -->
 		</nav>
 
