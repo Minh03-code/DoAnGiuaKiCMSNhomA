06fdae9c3b1869c742424f17671a3789d7a5ee3d
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-04-21 23:54:40 +0700

    z

--- wordpress/wp-content/themes/twentytwenty/header.php
+++ wordpress/wp-content/themes/twentytwenty/header.php
@@ -20,7 +20,6 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
 
 	<!-- Start Link is Header Moudel 1 -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -30,6 +29,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<!-- End Link is Header Moudel 1 -->
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
