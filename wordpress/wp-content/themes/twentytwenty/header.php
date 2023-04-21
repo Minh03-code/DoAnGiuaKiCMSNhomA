69f4d587c0a9ae8e101e36110a0fe1476ca59f9d
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-04-21 23:05:32 +0700

    edit comment để dễ tìm khi merge

--- wordpress/wp-content/themes/twentytwenty/header.php
+++ wordpress/wp-content/themes/twentytwenty/header.php
@@ -22,13 +22,14 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 
-	<!-- Link is Header Moudel 1 -->
+	<!-- Start Link is Header Moudel 1 -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<!-- End Link is Header Moudel 1 -->
 </head>
 
 <body <?php body_class(); ?>>
@@ -37,8 +38,7 @@
 	wp_body_open();
 	?>
 
-	<!-- Header Moudle 1 -->
-	<!-- Start -->
+	<!-- Start Header Moudle 1 -->
 	<header id="site-header" class="header-footer-group">
 		<?php
 		// Check whether the header search is activated in the customizer.
@@ -114,7 +114,7 @@
 		get_template_part('template-parts/modal-search');
 		?>
 	</header><!-- #site-header -->
-	<!-- End -->
+	<!-- End Header Moudle 1 -->
 
 	<?php
 	// Output the menu modal.
